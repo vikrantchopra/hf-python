cb3869d61d4464886ed3cc6ea15c7dbcb1729334
Vikrant Chopra <vchopra@onbase.net>
2019-06-06 17:37:41 +0530

    Some db insertions

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, escape
 from vsearch import search4letters
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -9,24 +10,49 @@ def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
-    results =  str(search4letters(phrase, letters))
+    results = str(search4letters(phrase, letters))
     log_request(request, results)
 
-    return render_template('results.html', 
-                            the_title = title, 
-                            the_results = results, 
-                            the_phrase=phrase, 
-                            the_letters=letters,)
+    return render_template('results.html',
+                           the_title=title,
+                           the_results=results,
+                           the_phrase=phrase,
+                           the_letters=letters,)
+
 
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
 
+
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
-        
+    # with open('vsearch.log', 'a') as log:
+        #print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+    dbconfig = {
+        'host': '127.0.0.1',
+        'user': 'vsearch',
+        'password': 'abcd1234',
+        'database': 'vsearchlogDB',
+    }
+    
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+
+    _SQL = """insert into log
+            (phrase, letters, ip, browser_string, results)
+            values
+            (%s, %s, %s, %s, %s)"""
+
+    cursor.execute(_SQL, (req.form['phrase'],
+                          req.form['letters'],
+                          req.remote_addr,
+                          req.user_agent.browser,
+                          res, ))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
 """
 @app.route('/viewlog')
 def view_the_log() -> str:
@@ -45,15 +71,16 @@ def view_the_log() -> 'html':
                 contents[-1].append(escape(item))
 
     titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
-    return render_template('viewlog.html', 
-                            the_title='View Log',
-                            the_row_titles=titles,
-                            the_data=contents,)
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents,)
 
 
 @app.route('/test')
 def view_test_result() -> 'html':
     return render_template('testresults.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
